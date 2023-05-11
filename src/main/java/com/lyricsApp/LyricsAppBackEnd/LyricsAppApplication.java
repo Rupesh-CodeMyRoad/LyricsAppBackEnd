9ab68f1c9a868d9450b3e39b470461eea401b29d
Rupesh-CodeMyRoad <rupeshgaudel3@gmail.com>
2023-05-11 07:59:32 +0545

    first commit

---
+++ src/main/java/com/lyricsApp/LyricsAppBackEnd/LyricsAppApplication.java
@@ -0,0 +1,13 @@
+package com.lyricsApp.LyricsAppBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyricsApp/LyricsAppBackEnd/LyricsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyricsApp.LyricsAppBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
